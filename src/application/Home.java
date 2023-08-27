37034bcf88d8dc8339e03e7f9f86a1b6e754489b
dazyb <dj734045@gmail.com>
2023-08-27 23:50:46 +0000

    updated

--- src/application/Home.java
+++ src/application/Home.java
@@ -1,23 +1,48 @@
 package application;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Home extends Stage{
-	public Home() {
-		StackPane root;
+public class Home extends Application{
+//	public Home() {
+//		StackPane root;
+//		try {
+//			root = FXMLLoader.load(getClass().getResource("home.fxml"));
+//			Scene scene = new Scene(root,1259,737);
+//			scene.getStylesheets().add(getClass().getResource("Home.css").toExternalForm());
+//			this.setResizable(false);
+//			this.setScene(scene);
+//			this.show();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+	@Override
+	public void start(Stage primaryStage) {
 		try {
-			root = FXMLLoader.load(getClass().getResource("home.fxml"));
+			StackPane root = (StackPane)FXMLLoader.load(getClass().getResource("home.fxml"));
 			Scene scene = new Scene(root,1259,737);
 			scene.getStylesheets().add(getClass().getResource("Home.css").toExternalForm());
-			this.setResizable(false);
-			this.setScene(scene);
-			this.show();
-		} catch (Exception e) {
+//			Image image=new Image(getClass().getResource("src/uccLogo.png").toURI().toString());
+//			primaryStage.getIcons().add(image);
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
