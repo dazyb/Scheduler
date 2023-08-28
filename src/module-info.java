e83c1007dc755a4dc6c01cebc661aecf53e4f124
dazyb <dj734045@gmail.com>
2023-08-28 00:23:14 +0000

    updated

--- src/module-info.java
+++ src/module-info.java
@@ -7,6 +7,8 @@ module Scheduler {
 	requires de.jensd.fx.glyphs.fontawesome;
 	requires javafx.base;
 	requires commons.configuration;
+	requires java.desktop;
+	requires org.apache.poi.ooxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens controller to javafx.graphics, javafx.fxml;
